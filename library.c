c87afad8b9ca71bd6799b482916d2d8b955ecb85
spocklogic <averykarlin3@gmail.com>
2015-10-16 13:33:05 -0400

    fix

--- library.c
+++ library.c
@@ -1,11 +1,9 @@
 #include "library.h"
 #include "list.h"
 
-/*
-void print_node(*node n) {
+void print_node(node* n) {
 	printf("%s - %s\n", n->name, n->artist);
 }
-*/
 
 node* add_song(node* table[], char n[], char a[]) {
 	return insert_lexic(table[a[0] - 'a'], n, a);
@@ -22,6 +20,5 @@ node* findlib_song(node* table[], char songn[]) {
 	}
 	return NULL;
 }
-
 
 
