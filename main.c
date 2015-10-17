6a7ab45aa49b22fbebb45192b6dc03ed0f2566db
spocklogic <averykarlin3@gmail.com>
2015-10-17 11:18:38 -0400

    Add song kinda not fix

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main() {
 	print_list(list);
 	print_list(find_song(list, "quiet"));
 	print_list(find_artist(list, "joshua"));
-	printf("Random List:\n");
+	printf("Random Node:\n");
 	print_list(randomn(list));
 	print_list(randomn(list));
 	print_list(randomn(list));
@@ -30,8 +30,10 @@ int main() {
 	add_song(table, "john", "fish");
 	add_song(table, "patrick", "star");
 	add_song(table, "picard", "jean-luc");
-	printf("Songs added\n");
+	printf("Songs added if not 0:\n");
+	printf("%p\n", table['p' - 'a']);
 	print_art(table, "john");
 	print_let(table, 'p');
+	printf("Shuffle List:\n");
 	return 0;
 }
